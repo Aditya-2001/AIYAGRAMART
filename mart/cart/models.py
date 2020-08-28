c8a794847032b27f270fd89bff00a834ebcaf184
Aditya-2001 <iit2019210@iiita.ac.in>
2020-08-28 23:13:19 +0530

    one model added

--- mart/cart/models.py
+++ mart/cart/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class CartItems(models.Model):
+    customer=models.ForeignKey(User, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE, default=0)
