c5d8d48b78c10fa17598b869e9b8224c6c49f66a
Aditya-2001 <iit2019210@iiita.ac.in>
2020-08-29 22:11:38 +0530

    lfvh

--- mart/cart/models.py
+++ mart/cart/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime,date
+from home.models import Product
 
 # Create your models here.
 class CartItems(models.Model):
--- mart/home/models.py
+++ mart/home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime,date
 
+
 # Create your models here.
 class Product(models.Model):
     name=models.CharField(max_length=1000, null=True)
