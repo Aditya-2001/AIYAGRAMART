0294f7e9ca8a9f9569d98049c605bdddfd07e561
Aditya-2001 <iit2019210@iiita.ac.in>
2020-08-30 14:40:01 +0530

    ejfnh

--- mart/cart/models.py
+++ mart/cart/models.py
@@ -8,3 +8,4 @@ class CartItems(models.Model):
     customer=models.ForeignKey(User, on_delete=models.CASCADE)
     product=models.ForeignKey(Product, on_delete=models.CASCADE, default=0)
     added_at=models.DateTimeField(auto_now_add=True)
+    total_quantity=models.IntegerField(default=1)
--- mart/cart/views.py
+++ mart/cart/views.py
@@ -28,4 +28,29 @@ def delete_from_cart(request):
         return redirect('home')
 
 def checkout_request(request):
-    return HttpResponse("djfh")
+    if request.method =="POST":
+        if request.user.is_authenticated:
+            products=CartItems.objects.filter(customer=request.user).order_by("-added_at")
+            
+            items={}
+            total_bill=0
+            for product in products:
+                if product.product.stock > 0:
+                    flag=1
+                    for key in items:
+                        if key==product.product.name:
+                            if items[key] < product.product.stock:
+                                items[key]=items[key] + 1
+                                total_bill=total_bill+product.product.price
+                            flag=0
+                            break  
+                    if flag==1:
+                        items.update({product.product.name:1})
+                        total_bill=total_bill+product.product.price
+                else:
+                    product.delete()
+            return render(request,"cart/checkout-page.html",context={"product": items, "total_bill": total_bill})
+        else:
+            return redirect('login_request')
+    else:
+        return HttpResponse("ERROR: Illegal method to order a product")
