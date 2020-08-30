d3f2514cbf60ae7ed1fe082438dff73f0326e078
Aditya-2001 <iit2019210@iiita.ac.in>
2020-08-30 15:12:19 +0530

    order placed

--- mart/cart/urls.py
+++ mart/cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import login_request_cart,home_cart,delete_from_cart,checkout_request
+from .views import login_request_cart,home_cart,delete_from_cart,checkout_request,confirm_order
 
 urlpatterns = [
     path('',login_request_cart,name="login_request_cart"),
     path('home_cart/',home_cart,name="home_cart"),
     path('delete_from_cart/',delete_from_cart,name="delete_from_cart"),
     path('checkout_request/',checkout_request,name="checkout_request"),
+    path('confrim_order/',confirm_order,name="confirm_order"),
 ]
--- mart/cart/views.py
+++ mart/cart/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import login,authenticate,logout
 from .models import CartItems
+from home.models import Orders,UsersOrders,Product
+from datetime import date
+
 # Create your views here.
 
 def login_request_cart(request):
@@ -50,6 +53,51 @@ def checkout_request(request):
                 else:
                     product.delete()
             return render(request,"cart/checkout-page.html",context={"product": items, "total_bill": total_bill})
+        else:
+            return redirect('login_request')
+    else:
+        return HttpResponse("ERROR: Illegal method to order a product")
+
+def confirm_order(request):
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            transaction_id=123
+            products=CartItems.objects.filter(customer=request.user).order_by("-added_at")
+            items={}
+            total_bill=0
+            for product in products:
+                if product.product.stock > 0:
+                    flag=1
+                    for key in items:
+                        if key==product.product.name:
+                            if items[key] < product.product.stock:
+                                items[key]=items[key] + 1
+                                total_bill=total_bill+product.product.price
+                            flag=0
+                            break  
+                    if flag==1:
+                        items.update({product.product.name:1})
+                        total_bill=total_bill+product.product.price
+                else:
+                    product.delete()
+
+            for product in products:
+                for key in items:
+                    if key==product.product.name:
+                        if  items[key]>0:
+                            items[key] = items[key] - 1
+                            Orders.objects.create(customer=request.user, product_ordered=product.product)
+                            UsersOrders.objects.create(customer=request.user, product_ordered=product.product)
+                            ID=product.product.id
+                            stock_ID=product.product.stock
+                            new=Product.objects.get(id=ID)
+                            new.stock = stock_ID - 1
+                            new.save()
+                            product.delete()
+                        else:
+                            product.delete()
+                        break
+            return HttpResponse("Order has been placed successfully")
         else:
             return redirect('login_request')
     else:
